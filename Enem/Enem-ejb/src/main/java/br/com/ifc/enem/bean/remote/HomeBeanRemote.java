ac09d082cc562ea32778c5a1b2d56529cbf70d0b
Fabricio Bizotto <fabricio.bizotto@gmail.com>
2017-03-14 14:58:36 -0300

    enem

---
+++ Enem/Enem-ejb/src/main/java/br/com/ifc/enem/bean/remote/HomeBeanRemote.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ifc.enem.bean.remote;
+
+import br.com.ifc.enem.entidades.Usuarios;
+import java.util.List;
+
+/**
+ *
+ * @author fabricio
+ */
+public interface HomeBeanRemote {
+
+    public String olaMundo() throws Exception;
+    public List<Usuarios> getUsuarios() throws Exception;
+}
